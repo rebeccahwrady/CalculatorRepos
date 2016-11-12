63081d0925b9cb79b670ab7b7ef3caec8eecfed6
Rebeccah Wrady <rebeccahwrady@gmail.com>
2016-11-12 08:22:37 -0600

    first content push

---
+++ controller.js
@@ -0,0 +1,52 @@
+var app = angular.module('myCalculator', [])
+
+app.controller('myCalculateCtrl', ['$scope', '$rootScope', function ($scope, $rootScope){
+        $scope.operand1 = 0;
+        $scope.operand2 = 0;
+        $scope.op1 = 0;
+        $scope.op2 = 0;
+        $scope.math = "adding";
+        $scope.word = "and";
+        $scope.result = 0;
+
+        $rootScope.add = function(){
+            $scope.result = parseInt($scope.operand1) + parseInt($scope.operand2);
+            $scope.math = "adding";
+            $scope.word = "and";
+            $scope.op1 = $scope.operand1;
+            $scope.op2 = $scope.operand2;
+            $scope.operand1 = 0;
+            $scope.operand2 = 0;
+        }
+
+        $rootScope.subtract = function(){
+            $scope.result = $scope.operand1 - $scope.operand2;
+            $scope.math = "subtracting";
+            $scope.word = "from";
+            $scope.op1 = $scope.operand2;
+            $scope.op2 = $scope.operand1;
+            $scope.operand1 = 0;
+            $scope.operand2 = 0;
+        }
+
+        $rootScope.multiply = function(){
+            $scope.result = $scope.operand1 * $scope.operand2;
+            $scope.math = "multiplying";
+            $scope.word = "by";
+            $scope.op1 = $scope.operand1;
+            $scope.op2 = $scope.operand2;
+            $scope.operand1 = 0;
+            $scope.operand2 = 0;
+        }
+
+        $rootScope.divide = function(){
+            $scope.result = $scope.operand1 / $scope.operand2;
+            $scope.math = "dividing";
+            $scope.word = "by";
+            $scope.op1 = $scope.operand1;
+            $scope.op2 = $scope.operand2;
+            $scope.operand1 = 0;
+            $scope.operand2 = 0;
+        }
+        
+    }])
