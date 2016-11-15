85b2115861217be982967e6e48a2641a9810529b
Rebeccah Wrady <rebeccahwrady@gmail.com>
2016-11-14 18:17:21 -0600

    final push

--- controller.js
+++ controller.js
@@ -10,43 +10,66 @@ app.controller('myCalculateCtrl', ['$scope', '$rootScope', function ($scope, $ro
         $scope.result = 0;
 
         $rootScope.add = function(){
-            $scope.result = parseInt($scope.operand1) + parseInt($scope.operand2);
-            $scope.math = "adding";
-            $scope.word = "and";
-            $scope.op1 = $scope.operand1;
-            $scope.op2 = $scope.operand2;
-            $scope.operand1 = 0;
-            $scope.operand2 = 0;
+            if($scope.operand1 == "" || $scope.operand2 == ""){
+                alert("You cannot leave your operands blank!!")
+            }
+            else{
+                $scope.result = parseInt($scope.operand1) + parseInt($scope.operand2);
+                $scope.math = "adding";
+                $scope.word = "and";
+                $scope.op1 = $scope.operand1;
+                $scope.op2 = $scope.operand2;
+                $scope.operand1 = 0;
+                $scope.operand2 = 0;
+            }
         }
 
         $rootScope.subtract = function(){
-            $scope.result = $scope.operand1 - $scope.operand2;
-            $scope.math = "subtracting";
-            $scope.word = "from";
-            $scope.op1 = $scope.operand2;
-            $scope.op2 = $scope.operand1;
-            $scope.operand1 = 0;
-            $scope.operand2 = 0;
+            if($scope.operand1 == "" || $scope.operand2 == ""){
+                alert("You cannot leave your operands blank!!")
+            }
+            else{
+                $scope.result = $scope.operand1 - $scope.operand2;
+                $scope.math = "subtracting";
+                $scope.word = "from";
+                $scope.op1 = $scope.operand2;
+                $scope.op2 = $scope.operand1;
+                $scope.operand1 = 0;
+                $scope.operand2 = 0;
+            }
         }
 
         $rootScope.multiply = function(){
-            $scope.result = $scope.operand1 * $scope.operand2;
-            $scope.math = "multiplying";
-            $scope.word = "by";
-            $scope.op1 = $scope.operand1;
-            $scope.op2 = $scope.operand2;
-            $scope.operand1 = 0;
-            $scope.operand2 = 0;
+            if($scope.operand1 == "" || $scope.operand2 == ""){
+                alert("You cannot leave your operands blank!!")
+            }
+            else{
+                $scope.result = $scope.operand1 * $scope.operand2;
+                $scope.math = "multiplying";
+                $scope.word = "by";
+                $scope.op1 = $scope.operand1;
+                $scope.op2 = $scope.operand2;
+                $scope.operand1 = 0;
+                $scope.operand2 = 0;
+            }
         }
 
         $rootScope.divide = function(){
-            $scope.result = $scope.operand1 / $scope.operand2;
-            $scope.math = "dividing";
-            $scope.word = "by";
-            $scope.op1 = $scope.operand1;
-            $scope.op2 = $scope.operand2;
-            $scope.operand1 = 0;
-            $scope.operand2 = 0;
+            if ($scope.operand2 == 0){
+                alert("Sorry, you cannot divide by zero!!");
+            }
+            else if($scope.operand1 == "" || $scope.operand2 == ""){
+                alert("You cannot leave your operands blank!!")
+            }
+            else{  
+                $scope.result = $scope.operand1 / $scope.operand2;
+                $scope.math = "dividing";
+                $scope.word = "by";
+                $scope.op1 = $scope.operand1;
+                $scope.op2 = $scope.operand2;
+                $scope.operand1 = 0;
+                $scope.operand2 = 0;
+            };
         }
         
     }])
